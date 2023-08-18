ea3f9f0ea169ca96a437956671ef1766056ebf43
Chien Nguyen <61793581+chiennv2000@users.noreply.github.com>
2023-08-18 17:43:16 +0700

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,7 +3,7 @@ if [ -z "$lang" ]
 then
     wget -r -np -nH --cut-dirs=2 --reject="index.html*" http://nlp.uoregon.edu/download/okapi-data/datasets
 else
-    wget -P ./datasets/multilingual-ranking-data-42k http://nlp.uoregon.edu/download/okapi-data/datasets/multilingual-alpaca-52k/$lang.json
+    wget -P ./datasets/multilingual-alpaca-52k http://nlp.uoregon.edu/download/okapi-data/datasets/multilingual-alpaca-52k/$lang.json
     wget -P ./datasets/multilingual-ranking-data-42k http://nlp.uoregon.edu/download/okapi-data/datasets/multilingual-ranking-data-42k/$lang.json
     wget -P ./datasets/multilingual-rl-tuning-64k http://nlp.uoregon.edu/download/okapi-data/datasets/multilingual-rl-tuning-64k/$lang.json
 fi
